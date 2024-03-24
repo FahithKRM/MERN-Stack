69510b6f84274b0053a6b9170d36fa0b04d99c2c
FahithKRM <FahithKRM>
2024-03-24 11:42:15 +0530

    Initial

---
+++ Contact-App/frontend/contact-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import AddAddress from './components/AddAddress';
+import AddressList from './components/AddressList';
+
+
+function App() {
+  return (
+    <>
+      <AddAddress />
+      <AddressList />
+    </>
+  );
+}
+
+export default App;
---
+++ Contact-App/frontend/contact-app/src/components/AddAddress.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {useNavigate} from 'react-router-dom';
+import Navigation from './Navigation';
+
+const AddAddress = () => {
+  return (
+    <div className="container" container-fluid min-vh-100 justify-content-center>
+        <h2 className="display-2 text-center">Add Address</h2>
+        
+    </div>
+  )
+}
+
+export default AddAddress
